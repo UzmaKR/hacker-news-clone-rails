6ce1afd76d68b04d506df0001e93b94c778b2fa4
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-06-24 21:22:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerNewsCloneRails::Application.config.secret_token = '31eb6a7797b097582bb118dc0c73f3a6c4d237f056dffc4d48ad1bc2f6b6fa2c60d2b9f95b7ce691d65b34edf318a2f4ae90a90a43a5658c7c886d7e9944e5d9'
